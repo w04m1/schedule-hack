17ad45b9461f410df0823a12f9cea62d9f2e090a
w04m1 <70951451+w04m1@users.noreply.github.com>
2024-08-05 06:30:31 +0200

    upd tm namescpace

--- tm-release/schedule-hack.user.js
+++ tm-release/schedule-hack.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Schedule Hack
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/w04m1/schedule-hack
 // @version      0.1.0
 // @description  Enjoy
 // @author       w04m1
@@ -9,31 +9,32 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    function getTermString() {
-        const date = new Date();
-        const year = date.getFullYear();
-        const month = date.getMonth();
-        const semester = month >= 6 ? 1 : 2;
-        const yearString = semester === 1 ? `${year}-${year + 1}` : `${year - 1}-${year}`;
-        return `${yearString}-${semester}`;
+  function getTermString() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const semester = month >= 6 ? 1 : 2;
+    const yearString =
+      semester === 1 ? `${year}-${year + 1}` : `${year - 1}-${year}`;
+    return `${yearString}-${semester}`;
+  }
+
+  function addOption() {
+    const termString = getTermString();
+    const options = document.getElementById('felev');
+    if (options) {
+      options.insertBefore(
+        new Option(termString, termString),
+        options.firstChild
+      );
+      options.value = termString;
+    } else {
+      console.error('Element with id "felev" not found.');
     }
+  }
 
-    function addOption() {
-        const termString = getTermString();
-        const options = document.getElementById('felev');
-        if (options) {
-            options.insertBefore(
-                new Option(termString, termString),
-                options.firstChild
-            );
-            options.value = termString;
-        } else {
-            console.error('Element with id "felev" not found.');
-        }
-    }
-
-    addOption();
+  addOption();
 })();
